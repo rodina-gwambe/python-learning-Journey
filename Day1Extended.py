69ce6b7b22e827191f560652d171e34bfec5274a
rodina-gwambe <rodinazeentlegwambe@gmail.com>
2026-07-24 00:55:13 +0200

    Day1Extended.py with variables and user input

---
+++ Day1Extended.py
@@ -0,0 +1,13 @@
+#Making a computer speak. Hastag is for writing a comment. The computer will ignore it. Just like // in Delphi. 
+print("Merhaba Hayatim")
+
+#Storing data inside a variable
+name = "Rodeena"
+print(name)
+
+#Making our code interactive. The computer will ask the user to write something and store it inside a variable.
+name = input("What is your name? ")
+print(f"Greetings {name}!")
+
+age = input("How old are you? ")
+print(f"You are {age} years old.")

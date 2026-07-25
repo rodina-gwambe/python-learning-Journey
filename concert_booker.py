c7354534a484c3a6263409d5aac521438a0a1e30
rodina-gwambe <rodinazeentlegwambe@gmail.com>
2026-07-25 22:57:17 +0200

    Add files via upload

---
+++ concert_booker.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+artist_or_band = input("Enter the name of the artist or band you want to see: ")
+print(f"Hey {name}! Your tickets to see {artist_or_band} are booked successfully!")
+
